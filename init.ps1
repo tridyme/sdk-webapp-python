3c98cf6001a61d8f5a1ef3629d07bfdc1d086b56
triazur <guillaume.cassin@triazur.com>
2025-03-24 09:47:05 +0100

    update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,13 +1,15 @@
 # Initialisation de l'environnement Python
-Write-Host "Initialisation de l'environnement Python..."
+Write-Host "Initialisation de l'environnement Python..." -ForegroundColor Cyan
 Set-Location backend
 python -m venv env
-. .\env\Scripts\activate
-pip install --upgrade pip
-pip install -r requirements.txt
+
+# Mise à jour de pip et installation des dépendances sans using activate
+Write-Host "Installation des dépendances Python..." -ForegroundColor Cyan
+.\env\Scripts\python.exe -m pip install --upgrade pip
+.\env\Scripts\pip.exe install -r requirements.txt
 
 # Initialisation de l'application React.js
-Write-Host "Initialisation de l'application React.js..."
+Write-Host "Initialisation de l'application React.js..." -ForegroundColor Cyan
 Set-Location ..\frontend
 npm install
 Set-Location module-federation
@@ -15,7 +17,9 @@ npm install
 Set-Location ../..
 
 # Build de l'application React.js
-Write-Host "Build de l'application React.js..."
+Write-Host "Build de l'application React.js..." -ForegroundColor Cyan
 Set-Location frontend
 npm run build
 Set-Location ..
+
+Write-Host "✅ Projet initialisé avec succès!" -ForegroundColor Green
